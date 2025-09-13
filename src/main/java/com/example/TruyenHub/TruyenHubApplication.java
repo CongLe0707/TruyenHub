8b1e943b073c351142fa99b6489908cd54943225
CongLe0707 <thanhcong772003@gmail.com>
2025-09-13 17:46:24 +0700

    Khởi đầu gian nan :))

---
+++ src/main/java/com/example/TruyenHub/TruyenHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.TruyenHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyenHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyenHubApplication.class, args);
+	}
+
+}
